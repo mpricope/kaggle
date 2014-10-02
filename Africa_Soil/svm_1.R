6778a5c9875cf4cfb97c83391a763a7a5a1c590c
Mihai Pricope <mihai.pricope@tirendo.com>
2014-10-02 19:17:35 +0200

    SOC         pH         Ca         P       Sand       All means    0.08790037 0.12575032 0.09019649 0.8860436 0.09790419 0.2575590 maxes    0.15565532 0.19659013 0.15300633 1.4107145 0.13648495 0.3627836 mins     0.03123737 0.09760348 0.03141207 0.1544617 0.06394976 0.1085252 deviance 1.41544279 0.78716817 1.34810413 1.4178228 0.74087931 0.9871851

--- Africa_Soil/svm_1.R
+++ Africa_Soil/svm_1.R
@@ -7,7 +7,8 @@ load('data/f.RData')
 #usedCols <- c(myData$mNames)
 usedCols <- c(myData$otherVars,myData$mNames,myData$dNames)
 
-thold <- list(SOC=0,pH=0,Ca=0,P=0,Sand=0)
+#thold <- list(SOC=0,pH=0,Ca=0,P=0,Sand=0)
+thold <- list(SOC=0.3,pH=0.1,Ca=0.15,P=0.05,Sand=0.3)
 #thold <- list(SOC=0.05,pH=0.05,Ca=0.05,P=0.05,Sand=0.05)
 
 result <- data.frame(PIDN = test$PIDN)
