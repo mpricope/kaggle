2d72545aa2a270bb7f85931c0680d38907767495
Miti <mpricope@gmail.com>
2014-10-11 14:40:57 +0200

    TradeShift: fixed, transformed - 0.0079579

--- TradeShift/testAlgo.py
+++ TradeShift/testAlgo.py
@@ -174,8 +174,9 @@ def runAlgo(alpha,nums,tCutoff,pCutoff):
 
     
 
+numsCol = [5, 6, 7, 8, 9,  16, 17, 18, 19, 20, 21, 22, 23, 28, 29, 36, 37, 38, 39, 40, 46, 47, 48, 49,50, 51, 52, 53, 54, 58, 59, 60, 66, 67, 68, 69, 70, 76, 77, 78, 79, 80, 81, 82,83, 84, 88, 89, 90, 96, 97, 98, 99, 100, 107, 108,109, 110, 111, 112, 113, 114, 119, 120, 121, 122, 123, 124, 125,  134, 137, 138, 139, 145]
 #numsCol = [5,6,7,8,9]
-numsCol = [5,6,7,8,9,29,28,17,16,19]
+
 #numsCol = [5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 27, 28, 29, 36, 37, 38, 39, 40, 46, 47, 48, 49, 50, 51, 52, 53, 54, 58, 59, 60, 66, 67, 68, 69, 70, 76, 77, 78, 79, 80, 81, 82, 83, 84, 88, 89, 90, 96, 97, 98, 99, 100, 106, 107, 108, 109, 110, 111, 112, 113, 114, 118, 119, 120, 121, 122, 123, 124, 125, 131, 132, 133, 134, 135, 136, 137, 138, 139, 143, 144, 145]
 #numsCol = [5,6,7,8,9,15, 16, 17, 18,19,21,28,29,36,37,38,39,40,47,50,52,59,60,66,67,68,69,70,77,80,82,89,90,96,97,98,99,100,107,110,112,119,120,121,122,123,124,125,132,135,137,144,145]
 #nums = [False, False, False, False, False, True, True, True, True, True, False, False, False, False, False, True, True, True, True, True, True, True, True, True, False, False, False, True, True, True, False, False, False, False, False, False, True, True, True, True, True, False, False, False, False, False, True, True, True, True, True, True, True, True, True, False, False, False, True, True, True, False, False, False, False, False, True, True, True, True, True, False, False, False, False, False, True, True, True, True, True, True, True, True, True, False, False, False, True, True, True, False, False, False, False, False, True, True, True, True, True, False, False, False, False, False, True, True, True, True, True, True, True, True, True, False, False, False, True, True, True, True, True, True, True, True, False, False, False, False, False, True, True, True, True, True, True, True, True, True, False, False, False, True, True, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]
@@ -187,46 +188,47 @@ for k in numsCol:
 #print ("Reference Original Train")
 #print (res)
 
-##testCols =  [  51, 52, 53, 54, 58, 59, 60, 66, 67, 68, 69, 70, 76, 77, 78, 79, 80, 81, 82, 83, 84, 88, 89, 90, 96, 97, 98, 99, 100, 106, 107, 108, 109, 110, 111, 112, 113, 114, 118, 119, 120, 121, 122, 123, 124, 125, 131, 132, 133, 134, 135, 136, 137, 138, 139, 143, 144, 145]
-#testCols =  [70, 76, 77, 78, 79, 80, 81, 82, 83, 84, 88, 89, 90, 96, 97, 98, 99, 100, 106, 107, 108, 109, 110, 111, 112, 113, 114, 118, 119, 120, 121, 122, 123, 124, 125, 131, 132, 133, 134, 135, 136, 137, 138, 139, 143, 144, 145]
-##testedCols = [15,16,17,18,19,20,21,22,23,27, 28, 29,36, 37, 38, 39, 40, 46, 47, 48, 49, 50,51, 52, 53, 54, 58, 59, 60, 66, 67, 68, 69] 
-#results = []
+##testCols =  [  15, 16, 17, 18, 19, 20, 21, 22, 23, 27, 28, 29, 36, 37, 38, 39, 40, 46, 47, 48, 49, 50, 51, 52, 53, 54, 58, 59, 60, 66, 67, 68, 69, 70, 76, 77, 78, 79, 80, 81, 82, 83, 84, 88, 89, 90, 96, 97, 98, 99, 100, 106, 107, 108, 109, 110, 111, 112, 113, 114, 118, 119, 120, 121, 122, 123, 124, 125, 131, 132, 133, 134, 135, 136, 137, 138, 139, 143, 144, 145]
+#testCols =  [   136, 137, 138, 139, 143, 144, 145]
+testCols =  [   136]
+##testedCols = [15, 16, 17, 18, 19, 20, 21, 22, 23, 27, 28, 29, 36, 37, 38, 39, 40, 46, 47, 48, 49,50, 51, 52, 53, 54, 58, 59, 60, 66, 67, 68, 69, 70, 76, 77, 78, 79, 80, 81, 82] 
+results = []
 #
 #
-#cutoff = .018281669974585812
-#
-#for c in testCols:
-#    print("Evaluate %d" % (c))
-#    testNumCols = numsCol[:]
-#    testNumCols.append(c)
-#    nums = [False] * 192
-#    for k in testNumCols:
-#        nums[k] = True
-#    (newLossT, newLossP) = runAlgo(.18,nums,100000,150000)
-#    #print("Results for %d are lossT=%.15f,lossP=%.15f" % (c,newLossT,newLossP))
-#    results.append([newLossP,newLossT,c])
-#    
-#    resultSorted = sorted(results, key=lambda tup: tup[0])
-#
-#    cutoffRiched = False    
-#    
-#    for i in resultSorted:
-#        if ((i[0] > cutoff) and (not cutoffRiched)):
-#            print("---------------- Cuttoff %15f riched-------------------" % (cutoff) )
-#            cutoffRiched = True         
-#        print(i)
-#
-#final = []
-#resultSorted = sorted(results, key=lambda tup: tup[0])
-#
-#for i in resultSorted:
-#    if (i[0] > cutoff):
-#        final.append(i[2])
-#
-#print(final)
+cutoff = .018276418575888315
 
-t = runAlgo(.18,nums,100000,150000)
-print(t)
+for c in testCols:
+    print("Evaluate %d" % (c))
+    testNumCols = numsCol[:]
+    testNumCols.append(c)
+    nums = [False] * 192
+    for k in testNumCols:
+        nums[k] = True
+    (newLossT, newLossP) = runAlgo(.18,nums,100000,150000)
+    #print("Results for %d are lossT=%.15f,lossP=%.15f" % (c,newLossT,newLossP))
+    results.append([newLossP,newLossT,c])
+    
+    resultSorted = sorted(results, key=lambda tup: tup[0])
+
+    cutoffRiched = False    
+    
+    for i in resultSorted:
+        if ((i[0] > cutoff) and (not cutoffRiched)):
+            print("---------------- Cuttoff %.15f riched-------------------" % (cutoff) )
+            cutoffRiched = True         
+        print(i)
+
+final = []
+resultSorted = sorted(results, key=lambda tup: tup[0])
+
+for i in resultSorted:
+    if (i[0] > cutoff):
+        final.append(i[2])
+
+print(final)
+
+#t = runAlgo(.18,nums,100000,150000)
+#print(t)
 
 print('Done, elapsed time: %s' % str(datetime.now() - start))
             
